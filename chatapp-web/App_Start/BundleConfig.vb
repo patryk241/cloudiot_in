d4da541e139519ada2497731413419a4902c942f
patryk241 <81463918+patryk241@users.noreply.github.com>
2022-01-31 23:13:28 +0100

    Add files via upload

---
+++ chatapp-web/App_Start/BundleConfig.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Web.Optimization
+
+Public Module BundleConfig
+    ' Aby uzyskać więcej informacji o grupowaniu, odwiedź stronę https://go.microsoft.com/fwlink/?LinkId=301862
+    Public Sub RegisterBundles(ByVal bundles As BundleCollection)
+
+        bundles.Add(New ScriptBundle("~/bundles/jquery").Include(
+                    "~/Scripts/jquery-{version}.js"))
+
+        bundles.Add(New ScriptBundle("~/bundles/jqueryval").Include(
+                    "~/Scripts/jquery.validate*"))
+
+        ' Użyj wersji deweloperskiej biblioteki Modernizr do nauki i opracowywania rozwiązań. Następnie, kiedy wszystko będzie
+        ' gotowe do produkcji, użyj narzędzia do kompilowania ze strony https://modernizr.com, aby wybrać wyłącznie potrzebne testy.
+        bundles.Add(New ScriptBundle("~/bundles/modernizr").Include(
+                    "~/Scripts/modernizr-*"))
+
+        bundles.Add(New ScriptBundle("~/bundles/bootstrap").Include(
+                  "~/Scripts/bootstrap.js"))
+
+        bundles.Add(New StyleBundle("~/Content/css").Include(
+                  "~/Content/bootstrap.css",
+                  "~/Content/site.css"))
+    End Sub
+End Module
+
---
+++ chatapp-web/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Informacje ogólne o zestawie zależą od poniższego
+' zestawu atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' związane z zestawem.
+
+' Przejrzyj wartości atrybutów zestawu
+<Assembly: AssemblyTitle("chatapp_web")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("chatapp_web")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+' Poniższy identyfikator GUID odpowiada atrybutowi ID biblioteki typów typelib, jeśli ten projekt jest uwidaczniany w modelu COM
+<Assembly: Guid("f2e20918-0c56-43da-a048-b9dd37a28fbf")>
+
+' Informacje o wersji zestawu obejmują następujące cztery wartości:
+'
+'      Wersja główna
+'      Wersja pomocnicza
+'      Numer kompilacji
+'      Poprawka
+'
+' Możesz określić wszystkie te wartości lub użyć wartości domyślnych numerów kompilacji i poprawki,
+' stosując znak „*”, jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
